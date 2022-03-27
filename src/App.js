2504ff69ce5674199e499b89ec3cb066c880f128
Joseph_mwangi <joseph.portuit@gmail.com>
2022-03-27 22:29:00 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// react function component
+
+const App = () => {
+    return (
+        <div>
+            <h1>Figma React App</h1>
+        </div>
+    )
+}
+
+export default App
