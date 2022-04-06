c39de95f6e0c1bac907fdfdaca494f8dad89c832
Joseph_mwangi <joseph.portuit@gmail.com>
2022-04-06 07:15:24 +0300

    init commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.svg';
 
 const navbar = () => {
     return (
@@ -11,7 +11,19 @@ const navbar = () => {
                     <img src={logo} alt="logo" />
 
                 </div>
+                <div className = "gpt3__navbar-links_container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt3">What is GPT3?</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="#features">Case studies</a></p>
+                    <p><a href="#blog">Library</a></p>
 
+                </div>
+
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sign in</p>
+                <button type="button">Sign Up</button>
             </div>
             
         </div>
